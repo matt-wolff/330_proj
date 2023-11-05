621e8d4747b99d45a274c052db655f164982f930
esKemp <40615223+esKemp@users.noreply.github.com>
2023-11-04 19:39:04 -0700

    updated embeddings.py

--- embeddings.py
+++ embeddings.py
@@ -2,15 +2,10 @@ import torch
 import esm
 
 # sequences: list of protein sequences
-def representations(sequences):
+def representations(data):
     model, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
     batch_converter = alphabet.get_batch_converter()
     model.eval()
-
-    # Prepare data 
-    data = []
-    for i, sequence in enumerate(sequences):
-        data.append((f'protein{i}', sequence))
 
     batch_labels, batch_strs, batch_tokens = batch_converter(data)
     batch_lens = (batch_tokens != alphabet.padding_idx).sum(1)
@@ -20,10 +15,17 @@ def representations(sequences):
         results = model(batch_tokens, repr_layers=[33], return_contacts=True)
     token_representations = results["representations"][33]
 
-    # Generate per-sequence representations via averaging
-    # NOTE: token 0 is always a beginning-of-sequence token, so the first residue is token 1.
-    sequence_representations = []
+    return token_representations, batch_lens
+
+def Embeddings2D(token_representations, batch_lens):
+    SeqReps2D = []
     for i, tokens_len in enumerate(batch_lens):
-        sequence_representations.append(token_representations[i, 1 : tokens_len - 1].mean(0))
-    
-    return sequence_representations
+        rep = token_representations[i, 1 : tokens_len - 1]
+        SeqReps2D.append(rep)
+    return SeqReps2D
+
+def Embeddings1D(token_representations, batch_lens):
+    SeqReps1D = []
+    for i, tokens_len in enumerate(batch_lens):
+        SeqReps1D.append(token_representations[i, 1 : tokens_len - 1].mean(0))
+    return SeqReps1D
