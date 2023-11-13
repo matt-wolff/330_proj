38731db15fcd55f73203fd5f1377042869eee28c
esKemp <40615223+esKemp@users.noreply.github.com>
2023-11-13 00:46:35 -0800

    added tensorboard support

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import json
 import ast
 import random
 import torch.nn.functional as F
+from torch.utils import tensorboard
+
+VAL_INTERVAL = 20
+
+log_dir = f'./logs/milestone_run_1'
+print(f'log_dir: {log_dir}')
+writer = tensorboard.SummaryWriter(log_dir=log_dir)
 
 # TRAIN_PATH = 'data/go_tasks.csv'
 # VAL_PATH = 'data/go_tasks.csv'
@@ -15,8 +22,8 @@ with open('data/residues.json', 'r') as f:
 df = pd.read_csv(PATH, encoding='utf-8')
 num_tasks, _ = df.shape # Note: _ = 3.
 # I chose a random split, we can modify this
-train_df = df[:int(0.6*num_tasks)]
-val_df = df[int(0.6*num_tasks):int(0.8*num_tasks)]
+train_df = df[:int(0.7*num_tasks)]
+val_df = df[int(0.7*num_tasks):int(0.8*num_tasks)]
 test_df = df[int(0.8*num_tasks):]
 learning_rates = [1e-6, 5e-6, 1e-5]
 num_epochs = 5 # Should we modify the number of epochs?
@@ -36,8 +43,67 @@ for lr in learning_rates:
             query_pos_ids = rand_pos_ids[5:]
             query_neg_ids = random.sample(neg_ids, k=3)
             probs = ball(support_ids, query_pos_ids + query_neg_ids)
-            targets = torch.Tensor([[1,0],[1,0],[1,0],[0,1],[0,1],[0,1]])
+            # targets = torch.Tensor([[0,1],[0,1],[0,1],[1,0],[1,0],[1,0]])
+            targets = torch.Tensor([1,1,1,0,0,0])
             loss = F.cross_entropy(torch.log(probs), labels)
             
             loss.backward()
             optimizer.step()
+
+            writer.add_scalar('loss/train', loss.item(), i_step)
+            accuracy_query = torch.mean((torch.argmax(probs,dim=1)==targets).float()).item()
+            writer.add_scalar(
+                'train_accuracy/query',
+                accuracy_query.item(),
+                i_step
+            )
+
+            # if i_step % self.val_interval == 0:
+            #     print("Start Validation...")
+            #     with torch.no_grad():
+            #         losses, accuracies = [], [], []
+            #         for i, val_task_batch in enumerate(dataloader_meta_val):
+            #             if self.bio and i > 600:
+            #                 break
+            #             loss, accuracy_support, accuracy_query = (
+            #                 self._step(val_task_batch)
+            #             )
+            #             losses.append(loss.item())
+            #             accuracies_support.append(accuracy_support)
+            #             accuracies_query.append(accuracy_query)
+            #         loss = np.mean(losses)
+            #         accuracy_support = np.mean(accuracies_support)
+            #         accuracy_query = np.mean(accuracies_query)
+            #         ci95 = 1.96 * np.std(accuracies_query) / np.sqrt(600 * 4)
+            #     if self.bio:
+            #         print(
+            #             f'Validation: '
+            #             f'loss: {loss:.3f}, '
+            #             f'support accuracy: {accuracy_support:.3f}, '
+            #             f'query accuracy: {accuracy_query:.3f}',
+            #             f'Ci95: {ci95:.3f}'
+            #         )
+            #     else: 
+            #         print(
+            #             f'Validation: '
+            #             f'loss: {loss:.3f}, '
+            #             f'support accuracy: {accuracy_support:.3f}, '
+            #             f'query accuracy: {accuracy_query:.3f}'
+            #         )
+            #     writer.add_scalar('loss/val', loss, i_step)
+            #     writer.add_scalar(
+            #         'val_accuracy/support',
+            #         accuracy_support,
+            #         i_step
+            #     )
+            #     writer.add_scalar(
+            #         'val_accuracy/query',
+            #         accuracy_query,
+            #         i_step
+            #     )
+            #     if self.bio:
+            #         writer.add_scalar(
+            #             'val_accuracy/ci95',
+            #             ci95,
+            #             i_step
+            #         )
