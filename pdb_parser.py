ac61fbba85c235a870e4c02bc08741894b0c200a
matt-wolff <mbwolff@stanford.edu>
2023-11-12 16:23:06 -0800

    Added GO functions to residues.csv.

--- pdb_parser.py
+++ pdb_parser.py
@@ -3,27 +3,46 @@ from Bio.Data.IUPACData import protein_letters_3to1
 from Bio.UniProt.GOA import gafiterator
 import os
 import pandas as pd
-DATA_PATH = 'data/UP000005640_9606_HUMAN_v4'
+DATA_PATH = 'data'
 
-parser = PDBParser()
+pdb_parser = PDBParser()
 data = {
-    "UnitProtKB accession number": [],
-    "Residues": []
+    "UniProtKB Object ID": [],
+    "Residues": [],
+    "GO Functions": []
 }
-for filename in os.listdir('./data/UP000005640_9606_HUMAN_v4'):
-    if filename.endswith(".pdb"):
-        structure = parser.get_structure('foobar', f'{DATA_PATH}/{filename}')
-        code_start = structure.header["name"].find("(") + 1
-        code_end = structure.header["name"].find(")")
-        uniprot = structure.header["name"][code_start:code_end]  # UniProtKB accession number
-        data['UnitProtKB accession number'].append(uniprot)
 
-        all_residues = ""
-        for chain in structure.get_chains():
-            res_list = Selection.unfold_entities(chain, "R")
-            for res in res_list:
+uniprot_to_functions = {}
+qualifiers = set()
+with open(f'{DATA_PATH}/goa_human.gaf', 'r') as handle:
+    for rec in gafiterator(handle):
+        if rec['Aspect'] == 'F' and rec['DB'] == 'UniProtKB' and 'NOT' not in rec["Qualifier"]:
+            if rec['DB_Object_ID'] in uniprot_to_functions.keys():
+                uniprot_to_functions[rec['DB_Object_ID']].add(rec['GO_ID'])
+            else:
+                uniprot_to_functions[rec['DB_Object_ID']] = {rec['GO_ID']}
+
+num_excluded_proteins = 0
+i = 0
+for filename in os.listdir(f'{DATA_PATH}/UP000005640_9606_HUMAN_v4'):
+    if filename.endswith(".pdb"):
+        uniprot = filename.split('-')[1]  # UniProtKB Object ID
+        if uniprot in uniprot_to_functions.keys():
+            data['UniProtKB Object ID'].append(uniprot)
+            data['GO Functions'].append(list(uniprot_to_functions[uniprot]))
+
+            pdb_structure = pdb_parser.get_structure('foobar', f'{DATA_PATH}/UP000005640_9606_HUMAN_v4/{filename}')
+            all_residues = ""
+            for res in pdb_structure.get_residues():
                 all_residues += protein_letters_3to1[res.get_resname().capitalize()]
-        data['Residues'].append(all_residues)
+            data['Residues'].append(all_residues)
+        else:
+            print(f"No functions for uniprotkb: {uniprot}")
+            num_excluded_proteins += 1
+        i += 1
+        if i == 10:
+            break
+print(f"Number of excluded proteins: {num_excluded_proteins}")
 
 pdb_df = pd.DataFrame(data)
 pdb_df.to_csv('data/residues.csv', index=False)
