bedc2eb1d1390a4284802eb3f7b4577e719c2422
matt-wolff <mbwolff@stanford.edu>
2023-11-12 20:36:17 -0800

    Added onto main.

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from torch.utils.data import DataLoader
+import pandas as pd
 import torch.optim as optim
-from model import ProteinEmbedder
+from model import ProteinEmbedder, knnClassifier
+import tqdm as tqdm
 
+TRAIN_PATH = 'data/residues.csv'
+VAL_PATH = 'data/residues.csv'
+
+train_df = pd.read_csv(TRAIN_PATH, encoding='utf-8')
+val_df = pd.read_csv(VAL_PATH, encoding='utf-8')
 train_dataset = ProteinDataset(train_df)
 val_dataset = ProteinDataset(val_df)
 learning_rates = [1e-6, 5e-6, 1e-5]
@@ -13,5 +20,10 @@ for batch_size in batch_sizes:
     val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True)
     for lr in learning_rates:
         embedder = ProteinEmbedder()
+        knn = knnClassifier(batch_size=16, k=2)  # output is [numExamples, 2]
         optimizer = optim.AdamW(embedder.parameters(), lr=lr)
         train_losses, val_losses = [], []
+        for epoch in range(num_epochs):
+            train_loss = 0
+            for batch in tqdm(train_dataloader, desc=f'Training epoch: {epoch}'):
+                optimizer.zero_grad()
